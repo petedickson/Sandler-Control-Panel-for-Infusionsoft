27884c4b03212264d6263094c9317e99b7584d68
Peter Dickson <peter@dalydickson.com>
2011-03-08 12:11:03 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SandlerControlPanel::Application.config.secret_token = 'b6a68dd118d07f1ab9865cc22a57c8f4af32b846806ee21921f9e1c6bef6e8753e385196007a504bdcaee6a903de8fbe6d1656190370d438772a703dff5b3b27'
